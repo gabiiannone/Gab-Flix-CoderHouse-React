ece05191554d9081ad4a3e3ee6c5c3537ddc6947
Gabriel Iannone <giannonearc@gmail.com>
2022-03-16 22:58:59 -0300

    creacion del NavBar con menu y logo

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Button from '@mui/material/Button'
+
+function NavBar() {
+    return(
+     <header className='mainHeader' >
+        <div className='containerLogo'>
+            <img className="imagenHeader" src="Logo1.png"  />
+        </div>
+        <ul className={'navbar'}>
+            <li> <Button variant="contained">Registrarse</Button> </li>
+            <li> <Button variant="contained">Iniciar Sesion</Button></li>
+            <li> <Button variant="contained" >Peliculas</Button></li>
+            <li> <Button variant="contained">Series</Button></li>
+            <li> <Button variant="contained" >Video juegos</Button></li>
+        </ul>
+       
+     </header>
+    )
+
+
+}
+export default NavBar
